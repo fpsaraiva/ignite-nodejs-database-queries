302bbb741db2ebb9b6c6b6fd445ed87fcd5bcbda
Fernando Saraiva <fpsaraiva@gmail.com>
2021-05-06 11:57:11 -0300

    challenge solved

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,26 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
+    // Complete usando query builder
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games").where(`games.title ILIKE '%${param}%'`).getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) FROM games`); 
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
+    // Complete usando query builder
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games").innerJoin("games.users", "users").getOne()
+      .then(game => {
+        if(!game) {
+          throw new Error("Game not found!");
+        }
+        return game.users;
+      })  
   }
 }
+ 
